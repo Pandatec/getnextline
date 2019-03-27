81e626de2e6796ce964b7050458c84b357bd751c
Pandatec <48299373+Pandatec@users.noreply.github.com>
2019-03-27 14:58:44 -0400

    Add files via upload

---
+++ get_next_line.h
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2019
+** get_next_line.h
+** File description:
+** define of macro and fonction prototype
+*/
+
+#ifndef GET_NEXT_LINE_H_
+#define GET_NEXT_LINE_H_
+
+//static const int READ_SIZE = 5;
+#ifndef READ_SIZE
+#define READ_SIZE (5)
+#endif /* READ_SIZE */
+
+char *get_next_line(int);
+
+#endif /* GET_NEXT_LINE_H_ */
